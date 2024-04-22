622408588610a2054625266adf08a21333d11d4d
Linghua Zhang <linghua.zhang@me.com>
2024-04-22 21:08:39 +0900

    chore: upgrade to v2, nodejs 20

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,10 @@
 'use strict';
 
-const uuid = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 const moment = require('moment');
 
-const whitelist = [ 'https://lhzhang.com' ];
-const cors = require('cors')({
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-});
-const functions = require('firebase-functions');
+const whitelist = ['https://lhzhang.com'];
+const functions = require('firebase-functions/v2');
 const admin = require('firebase-admin');
 const serviceAccount = require("./account_key.json");
 admin.initializeApp({
@@ -23,86 +14,72 @@ admin.initializeApp({
 
 const db = admin.database();
 
-function onCount(req, res) {
+async function onCount(req, res) {
   const { identifier, shortname } = req.query;
   if (!identifier || !shortname) {
     return res.status(400).send('Bad Request');
   }
 
-  const ref = [ shortname, 'entries', identifier, 'users' ].join('/');
+  const ret = {
+    liked: false,
+    count: 0,
+    user: req.query.user || uuidv4()
+  };
 
-  db.ref(ref)
-    .once('value')
-    .then((snapshot) => {
-      const ret = {
-        liked: false,
-        count: 0,
-        user: req.query.user || uuid.v4()
-      };
+  const ref = [shortname, 'entries', identifier, 'users'].join('/');
+  const val = (await db.ref(ref).once('value')).val();
+  if (!val) {
+    return res.status(200).json(ret);
+  }
 
-      const val = snapshot.val();
-      if (!val) {
-        return res.status(200).json(ret);
-      }
+  ret.count = val.length;
+  ret.liked = val.indexOf(ret.user) >= 0;
 
-      ret.count = val.length;
-      ret.liked = val.indexOf(ret.user) >= 0;
-      res.status(200).json(ret);
-    });
+  res.status(200).json(ret);
 }
 
-function onLike(req, res) {
+async function onLike(req, res) {
   const { shortname, identifier, name, link, user } = req.body;
-  if (!shortname || ! identifier || !name || !link || !user) {
+  if (!shortname || !identifier || !name || !link || !user) {
     return res.status(400).send('Bad Request');
   }
 
-  const ref = [ shortname, 'entries', identifier ].join('/');
-  db.ref(ref)
-    .once('value')
-    .then((snapshot) => {
-      let val = snapshot.val() || {
-        title: name,
-        url: link
-      };
-      val.users = val.users || [ ];
+  const ref = [shortname, 'entries', identifier].join('/');
+  const val = (await db.ref(ref).once('value')).val() || {
+    title: name,
+    url: link
+  };
+  val.users = val.users || [];
 
-      return Promise.resolve(val);
-    })
-    .then((val) => {
-      const idx = val.users.indexOf(user);
-      if (idx >= 0) {
-        val.users.splice(idx, 1);
-      } else {
-        val.users.push(user);
-      }
+  const idx = val.users.indexOf(user);
+  if (idx >= 0) {
+    val.users.splice(idx, 1);
+  } else {
+    val.users.push(user);
+  }
+  const liked = idx < 0;
 
-      const liked = idx < 0;
+  await db.ref(ref).set(val);
+  await log(shortname, (liked ? 'Liked' : 'Unliked') + ' ' + name);
 
-      const retPromise = Promise.resolve({
-        user: user,
-        liked: liked,
-        count: val.users.length
-      });
-      const savePromise = db.ref(ref).set(val);
-      const logPromise = log(shortname, (liked ? 'Liked' : 'Unliked') + ' ' + name );
-
-      return Promise.all([ retPromise, savePromise, logPromise ]);
-    })
-    .then(([ ret ]) => {
-      res.status(200).json(ret);
-    });
+  res.status(200).json({
+    user: user,
+    liked: liked,
+    count: val.users.length
+  });
 }
 
 function log(shortname, msg) {
-  const ref = [ shortname, 'logs' ].join('/');
+  const ref = [shortname, 'logs'].join('/');
   return db.ref(ref).push(moment().format() + ' ' + msg);
 }
 
-exports.count = functions.https.onRequest((req, res) => {
-  cors(req, res, () => { onCount(req, res); });
-});
+exports.count = functions.https.onRequest(
+  { cors: whitelist },
+  async (req, res) => { await onCount(req, res); }
+);
 
-exports.like = functions.https.onRequest((req, res) => {
-  cors(req, res, () => { onLike(req, res); });
-});
+exports.like = functions.https.onRequest(
+  { cors: whitelist },
+  (req, res) => { onLike(req, res); }
+);
